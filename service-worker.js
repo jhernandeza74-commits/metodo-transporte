abc6579c4dc46cc50832748eeaaee5e6aa5330ea
jhernandeza74-commits <jhernandeza74@miumg.edu.gt>
2025-10-11 11:27:49 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("transporte-cache").then(cache => {
       return cache.addAll([
-        "index.html",
+        "index-viejo.html",
         "manifest.json",
         "icon-192.png",
         "icon-512.png"
